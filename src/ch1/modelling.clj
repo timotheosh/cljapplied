d9bb717e7cfaaa5d13ae9858b59ee7e546e0a472
Tim Hawes <tim.hawes@inin.com>
2016-08-31 22:11:14 -0400

    Progress for chapter 1

--- src/ch1/modelling.clj
+++ src/ch1/modelling.clj
@@ -1,6 +1,19 @@
 (ns ch1.modelling)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord Planet [name
+                   moons
+                   volume     ;; km^3
+                   mass       ;; kg
+                   aphelion   ;; km, farthest from sun
+                   perihelion ;; km, closest to sun
+                   ])
+
+(def earth
+  (map->Planet {:name       "Earth"
+                :moons      1
+                :volume     1.08321e12   ;; km^3
+                :mass       5.97219e24   ;; kg
+                :aphelion   152098232    ;; km, farthest from sun
+                :perihelion 147098290    ;; km, closest to sun
+                :type       :Planet      ;; entity type
+                }))
